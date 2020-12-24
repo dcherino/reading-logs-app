1c09b44332c1b51391a3bbfd02c4521c59885683
Daniel Cherino <daniel@cherino.me>
2020-12-24 03:44:54 +0100

    Serving static files from client public folder

--- server/index.js
+++ server/index.js
@@ -50,6 +50,16 @@ app.get(endpoint, async (req, res) => {
     .catch((error) => res.status(500).send(error));
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
