35caa38e6ee79edce98c25aef4c7e01cbed55edb
Daniel Cherino <daniel@cherino.me>
2020-12-24 06:01:48 +0100

    Refactoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,64 +1,61 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { fetchLogs, logsSelector } from './app/slices';
-import TableDisplay from './components/TableDisplay';
+import React from 'react';
+import Table from './components/Table';
 import Grid from '@material-ui/core/Grid';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
+import Header from './components/Header';
 import Chart from './components/Chart';
+import TotalLogs from './components/TotalLogs';
+import NumberOcurrences from './components/NumberOcurrences';
 // import './App.css';
 
 function App() {
-  const dispatch = useDispatch();
-  const { loading, hasErrors } = useSelector(logsSelector, shallowEqual);
-  const [hasRender, setHasRender] = useState(false);
-
-  useEffect(() => {
-    dispatch(fetchLogs());
-
-    if (!hasErrors) {
-      setHasRender(true);
-    }
-
-    // Periodical fetching
-    const interval = setInterval(() => {
-      dispatch(fetchLogs());
-    }, 10000);
-
-    // Clear interval
-    return () => clearInterval(interval);
-  }, [dispatch, hasErrors]);
-
-  const renderLogs = () => {
-    if (loading && !hasRender) return <p>Loading logs...</p>;
-    if (hasErrors) return <p>Cannot display logs..</p>;
-
-    return <TableDisplay />;
-  };
-
   return (
     <div className="App">
       <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <AppBar position="static">
-            <Toolbar variant="dense">
-              <Typography variant="h5" color="inherit">
-                Alevo Logs
-              </Typography>
-            </Toolbar>
-          </AppBar>
+        <Grid item xs={12} style={{ padding: 0 }}>
+          <Header />
         </Grid>
-        <Grid container spacing={3} style={{maxWidth: '1800px', margin: '0 auto'}}>
-          <Grid item xs={8}>
-            {renderLogs()}
+        <Grid
+          item
+          xs={12}
+          style={{
+            padding: 0,
+            backgroundImage:
+              'linear-gradient(to left bottom,#4502b3,#7209c4,#9915d4,#bf22e3,#e530f1)',
+            height: '300px',
+          }}
+        >
+          <h1>Logs Tracker</h1>
+        </Grid>
+        <Grid
+          container
+          spacing={3}
+          style={{ maxWidth: '1800px', margin: '0 auto' }}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={4}>
+              <Card>
+                <TotalLogs />
+              </Card>
+            </Grid>
+            <Grid item xs={4}>
+              <Card style={{ padding: '20px' }}>
+                <NumberOcurrences />
+              </Card>
+            </Grid>
+            <Grid item xs={4}>
+              <Card style={{ padding: '20px' }}>
+                <Chart />
+              </Card>
+            </Grid>
           </Grid>
-          <Grid item xs={2}>
-            <Card>
-              <Chart />
-            </Card>
+          <Grid item xs={12}>
+            <Table />
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs={12} style={{ maxWidth: '1800px', margin: '0 auto' }}>
+            &copy; 2020 Alveo Tech. All rights reserved.
           </Grid>
         </Grid>
       </Grid>
